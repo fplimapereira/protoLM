96f8415fd726bee7ddb57e32c1ba07c1bb9e7c50
Felipe P <fp.limapereira@gmail.com>
2019-10-22 15:32:03 -0300

    Alterado factory para incluir objeto do tipo repositorio de personagem

--- app/src/main/java/com/example/protolm/factory/CharacterViewModelFactory.kt
+++ app/src/main/java/com/example/protolm/factory/CharacterViewModelFactory.kt
@@ -3,13 +3,14 @@ package com.example.protolm.factory
 import android.app.Application
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import com.example.protolm.repository.CharacterRepository
 import com.example.protolm.viewmodel.CharacterViewModel
 
-class CharacterViewModelFactory(private val app: Application): ViewModelProvider.Factory{
+class CharacterViewModelFactory(private val charRepo: CharacterRepository): ViewModelProvider.Factory{
     @Suppress("unchecked_cast")
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(CharacterViewModel::class.java)){
-            return CharacterViewModel(app) as T
+            return CharacterViewModel(charRepo) as T
         }
         throw IllegalArgumentException("Unknow viewModel class")
     }
