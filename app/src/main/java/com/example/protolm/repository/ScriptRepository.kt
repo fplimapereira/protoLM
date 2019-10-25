49c5af6e0b8b5ed4a0992a67151cdcbad04ca94c
Felipe P <fp.limapereira@gmail.com>
2019-10-25 17:10:09 -0300

    Adicionado layouts de fragment_script e battle_drawer

--- app/src/main/java/com/example/protolm/repository/ScriptRepository.kt
+++ app/src/main/java/com/example/protolm/repository/ScriptRepository.kt
@@ -1,26 +1,6 @@
 package com.example.protolm.repository
 
 import android.app.Application
-import com.example.protolm.model.entities.Character
-import com.example.protolm.model.entities.Prefs
-
-class ScriptRepository(val app: Application){
-
-    val pref: Prefs = Prefs(app)
 
 
-
-
-
-
-
-
-
-
-
-
-
-    fun saveChar(char: Character){
-        pref.saveChar(char)
-    }
-}
+class ScriptRepository(val app: Application)
