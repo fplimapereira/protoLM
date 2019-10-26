f8b20a942933c720644faf2f43b968d709f33521
Felipe P <fp.limapereira@gmail.com>
2019-10-26 18:57:45 -0300

    Implementando fragment de script. Falta solucionar problema de referencia em databinding

--- app/src/main/java/com/example/protolm/view/ScriptFragment.kt
+++ app/src/main/java/com/example/protolm/view/ScriptFragment.kt
@@ -1,24 +1,72 @@
 package com.example.protolm.view
 
-
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 import com.example.protolm.R
+import com.example.protolm.factory.ScriptViewModelFactory
+import com.example.protolm.factory.SwitcherFactory
+import com.example.protolm.viewmodel.ScriptViewModel
+import com.example.protolm.databinding.ScriptFragmentBinding
 
 
+class ScriptFragment: Fragment() {
 
-class ScriptFragment : Fragment() {
+    lateinit var binding: ScriptFragmentBinding
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_script, container, false)
+
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_script, container, false)
+
+        val application = requireNotNull(this.activity).application
+        val viewModelFactory = ScriptViewModelFactory(application)
+        val scriptViewModel = ViewModelProviders.of(this, viewModelFactory).get(ScriptViewModel::class.java)
+        binding.tvSwitch.setFactory {  SwitcherFactory(application).makeView() }
+        scriptViewModel.getEvento(1)
+        binding.viewModel = scriptViewModel
+        scriptViewModel.enredo.observe(this, Observer {
+            binding.tvSwitch.setText(it.evento)
+            binding.radioButton.text = it.acaoUm
+            binding.radioButton.isChecked = false
+
+            if(it.acaoDois != null){
+                binding.radioButton2.visibility = View.VISIBLE
+                binding.radioButton2.text = it.acaoDois
+                binding.radioButton2.isChecked = false
+            }
+            else{binding.radioButton2.visibility = View.GONE}
+
+            if(it.acaoTres != null){
+                binding.radioButton3.visibility = View.VISIBLE
+                binding.radioButton3.text = it.acaoTres
+                binding.radioButton3.isChecked = false
+            }
+            else{binding.radioButton3.visibility = View.GONE}
+
+            if(it.acaoQuatro != null){
+                binding.radioButton4.visibility = View.VISIBLE
+                binding.radioButton4.text = it.acaoQuatro
+                binding.radioButton4.isChecked = false
+            }
+            else{binding.radioButton4.visibility = View.GONE}
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+            //it.efeitoId?.let { binding.viewModel. }
+
+        })
+
+        return binding.root
     }
-
-
 }
+
+
+
+
