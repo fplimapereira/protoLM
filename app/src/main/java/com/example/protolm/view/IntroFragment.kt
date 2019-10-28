745fa08ae0f129bda79411d40e4e9b0331e5741f
Felipe P <fp.limapereira@gmail.com>
2019-10-28 20:29:45 -0300

    implementado navegacao de introFragment para ScriptFragment

--- app/src/main/java/com/example/protolm/view/IntroFragment.kt
+++ app/src/main/java/com/example/protolm/view/IntroFragment.kt
@@ -7,9 +7,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
 import com.example.protolm.R
 import com.example.protolm.databinding.IntroFragmentBinding
-
+import com.example.protolm.view.IntroFragmentDirections.actionIntroFragmentToScriptFragment
 
 
 class IntroFragment : Fragment() {
@@ -19,6 +20,10 @@ class IntroFragment : Fragment() {
 
         val binding : IntroFragmentBinding = DataBindingUtil.inflate(
             inflater, R.layout.intro_fragment, container, false)
+
+        binding.button.setOnClickListener {
+            it.findNavController().navigate(actionIntroFragmentToScriptFragment())
+        }
 
         return binding.root
     }
