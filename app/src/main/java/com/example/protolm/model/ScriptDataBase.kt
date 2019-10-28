83692e8e100b2dc89a57be59f67b92c4c6c4192b
Felipe P <fp.limapereira@gmail.com>
2019-10-28 20:08:55 -0300

    Implementado classe para banco de dados

---
+++ app/src/main/java/com/example/protolm/model/ScriptDataBase.kt
@@ -0,0 +1,41 @@
+package com.example.protolm.model
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.protolm.dao.EventosDao
+import com.example.protolm.dao.InimigosDao
+import com.example.protolm.dao.ItensDao
+import com.example.protolm.model.entities.Eventos
+import com.example.protolm.model.entities.Inimigos
+import com.example.protolm.model.entities.Itens
+
+@Database(entities = [Eventos::class, Inimigos::class, Itens::class], version = 1)
+abstract class ScriptDataBase : RoomDatabase(){
+
+    abstract fun eventosDao(): EventosDao
+    abstract fun inimigosDao(): InimigosDao
+    abstract fun itensDao(): ItensDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: ScriptDataBase? = null
+
+        fun getInstance (context: Context): ScriptDataBase{
+            synchronized(this){
+                var instance = INSTANCE
+
+                if(instance == null){
+                    instance = Room.databaseBuilder(context.applicationContext,
+                        ScriptDataBase::class.java, "bancolm")
+                        .fallbackToDestructiveMigration()
+                        .createFromAsset("bancolm.db")
+                        .build()
+                        INSTANCE = instance
+                }
+                return instance
+            }
+        }
+    }
+}
