cf24df52af8f0ef44b15da47e94f4469ab958ea0
Felipe P <fp.limapereira@gmail.com>
2019-10-30 15:56:49 -0300

    Implementado metodo para atualizacao de itens do persnagem

--- app/src/main/java/com/example/protolm/repository/ScriptRepository.kt
+++ app/src/main/java/com/example/protolm/repository/ScriptRepository.kt
@@ -1,6 +1,23 @@
 package com.example.protolm.repository
 
 import android.app.Application
+import com.example.protolm.dao.EventosDao
+import com.example.protolm.dao.InimigosDao
+import com.example.protolm.dao.ItensDao
+import com.example.protolm.model.ScriptDataBase
+import com.example.protolm.model.entities.Eventos
+import com.example.protolm.model.entities.Itens
 
 
-class ScriptRepository(val app: Application)
+class ScriptRepository(val app: Application){
+
+    private val eventoDao: EventosDao = ScriptDataBase.getInstance(app).eventosDao()
+    private val itemDao: ItensDao = ScriptDataBase.getInstance(app).itensDao()
+    private val inimigosDao: InimigosDao = ScriptDataBase.getInstance(app).inimigosDao()
+
+
+    fun getEvento(id: Int): Eventos? = eventoDao.getScript(id)
+
+    fun updateItem(item: Itens) = itemDao.updateItem(item)
+
+}
