d057e2573db62ac9a07ff4f9595b4522612511d8
Felipe de Lima Pereira <fp.limapereira@gmail.com>
2019-11-11 11:24:58 -0300

    Insercao de objeto de repositorio de character para acesso atributos do usuario

--- app/src/main/java/com/example/protolm/factory/TestViewModelFactory.kt
+++ app/src/main/java/com/example/protolm/factory/TestViewModelFactory.kt
@@ -2,12 +2,13 @@ package com.example.protolm.factory
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import com.example.protolm.repository.CharacterRepository
 import com.example.protolm.viewmodel.TestViewModel
 
-class TestViewModelFactory (private val id: Int): ViewModelProvider.Factory{
+class TestViewModelFactory (private val id: Int, private val charRepo: CharacterRepository): ViewModelProvider.Factory{
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(TestViewModel::class.java)){
-            return TestViewModel(id) as T
+            return TestViewModel(id, charRepo) as T
         }
         throw IllegalArgumentException("Unknow viewModel class")
     }
